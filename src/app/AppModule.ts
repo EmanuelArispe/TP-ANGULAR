119dd814e1d2045d0fcec5937a99fc0a64e7fe88
Emanuel Arispe <ema.arispe@gmail.com>
2024-06-30 18:00:47 -0300

    Se agrega pipe al observable que retorna la peticion a la api

--- src/app/AppModule.ts
+++ src/app/AppModule.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolsListComponent } from './tools-list/tools-list.component';
-
 import { FormsModule } from '@angular/forms';
 import { ToolsAboutComponent } from './tools-about/tools-about.component';
 import { ToolsCompraComponent } from './tools-compra/tools-compra.component';
 import { ToolsCartComponent } from './tools-cart/tools-cart.component';
 import { InputNumberComponent } from './input-number/input-number.component';
-import { HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -25,9 +24,10 @@ import { HttpClient } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClient
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/service/tool-data.service.ts
+++ src/app/service/tool-data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { Tools } from '../tools-list/Tools';
 
 const URL = 'https://66819b3404acc3545a07276d.mockapi.io/api/v1/article';
@@ -13,7 +13,11 @@ export class ToolDataService {
   constructor( private http:HttpClient) { }
 
   public getAll(): Observable<Tools[]>{
-  return this.http.get<Tools[]>(URL);
+
+  return this.http.get<Tools[]>(URL)
+              .pipe(
+                tap((tools : Tools[]) => tools.forEach(tool => tool.quantity = 0))
+              );
   }
 
 }
